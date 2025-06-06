9ee828d8e39cd2ec3e9fe6f569550eeb56f7a67e
dterenin <dterenin@yandex.ru>
2025-06-06 12:00:45 +0300

    fix fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,9 +6,14 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+	'./src/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
   	extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)"],
+        mono: ["var(--font-geist-mono)"],
+      },
   		colors: {
   			primary: {
   				DEFAULT: 'hsl(var(--primary))',
@@ -69,7 +74,6 @@ const config: Config = {
   	}
   },
   plugins: [
-    require('@tailwindcss/forms'),
     require('tailwindcss-animate'),
   ],
 }
