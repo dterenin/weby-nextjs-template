1c10d352e3a23f86420901866b65a3c9f82d2a4f
dterenin <dterenin@yandex.ru>
2025-06-07 11:04:29 +0300

    update use-mobile.ts

--- hooks/use-mobile.ts
+++ hooks/use-mobile.ts
@@ -2,10 +2,14 @@ import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
-export function useIsMobile() {
+export function useMobile() { 
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
   React.useEffect(() => {
+    if (typeof window === 'undefined') {
+      return;
+    }
+
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
     const onChange = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
